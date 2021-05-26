401cfffa82273265c7723c3d996f1e8193cfd4e6
Düzgün İLASLAN <74202972+duzgunilaslan@users.noreply.github.com>
2021-05-26 21:50:40 +0300

    Add files via upload

--- my_site/myapp/views.py
+++ my_site/myapp/views.py
@@ -3,40 +3,69 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import PyPDF2 as pdf
-import textract as tt
 import re
-import nltk
+import numpy as np
+import re
+from keras.preprocessing.text import Tokenizer 
+from keras.preprocessing.sequence import pad_sequences       
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed
+from tensorflow.keras.models import Model
+from keras import backend as K 
+from .attention import AttentionLayer
+
 
 
 def index(request):
-    context = {'a':'Hello Word'}
+    context = {'a':'Hello World'}
     return render(request,'index.html',context)
 # Create your views here.
-articles =""
+articles = ""
+temp={}
+
+count =0
 def fullarticle(request):
    context = {'article':''}
+
+
    if request.method == 'POST':
-       temp={}
+       
+
+
        temp['textArea']=request.POST.get('textArea')
        temp['file']=request.POST.get('file')
        context = {}
        clean_article=[]
        clean_textareaArticle=[]
-       # doc = ''
+       result = []
+       full_article=[]
+
+       # if click the summary button evet
        if request.POST.get('getSummary'):
-            print(temp['textArea'])
-            print(temp['file'])
+            #print(temp['textArea'])
+            #print(temp['file'])
             global articles
+            global count
             
-            if temp['textArea'] !="" and temp['file'] !="":
+            #if textarea and .pdf file is full condition
+            if temp['textArea'] !="" and temp['file'] is None:
                 temp['message'] ="You can choose just one choice .pdf or text"
-            elif temp['textArea'] != "" and temp['file'] == "":
+            #if textarea is full
+            if temp['textArea'] != "" and temp['file'] == "":
                 textareaArticle =  temp['textArea']  
                 clean_textareaArticle.append(cleanText(textareaArticle,removeStopwords=False))
-                temp['CleantextArea'] = clean_textareaArticle 
-            if temp['file'] is None and temp['textArea'] == "        ":  
+                global count
+                #split the article for model
+                for i in range(0, len(clean_textareaArticle[0]), 3000):
+                    slice_item = slice(i,i+3000)
+                    result.append(clean_textareaArticle[0][slice_item])
+                    count=count+1
+                temp['textAreaNew'] = clean_textareaArticle
+                #count = count - 1
+
+            # if textarea is null .pdf is upload 
+            if temp['file'] is None and temp['textArea'] == "":  
                 uploaded_file = request.FILES['file']
-                print(uploaded_file.name)
+                print(uploaded_file.name+" inside if")
                 fs = FileSystemStorage()
                 print(fs)
                 global name
@@ -48,7 +77,172 @@ def fullarticle(request):
                 
                 articles = pdf_To_text(doc)
                 clean_article.append(cleanText(articles, removeStopwords=False))
-                temp['article'] = clean_article  
+                
+                #split the article for model
+                for i in range(0, len(clean_article[0]), 3000):
+                    slice_item = slice(i,i+3000)
+                    result.append(clean_article[0][slice_item])
+                    count=count+1
+                temp['newpdf'] = clean_article
+                #count = count - 1
+
+            #preparing a tokenizer for summary on training data 
+            text_tokenizer = Tokenizer()
+            text_tokenizer.fit_on_texts(list(result))
+            #convert summary sequences into integer sequences
+            result    =   text_tokenizer.texts_to_sequences(result) 
+            #padding zero upto maximum length
+            result    =   pad_sequences(result, maxlen=600, padding='post')
+            text_voc_size  =   len(text_tokenizer.word_index) +1    
+
+
+
+
+            latent_dim = 220
+            #embedding_dim=200
+            max_text_length=600
+            # Encoder
+            encoder_inputs = Input(shape=(max_text_length,))
+
+            #embedding layer
+            enc_emb =  Embedding(text_voc_size, latent_dim,trainable=True)(encoder_inputs)
+
+            #encoder lstm 1
+            encoder_lstm1 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+            encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+
+            #encoder lstm 2
+            encoder_lstm2 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+            encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+            #encoder lstm 3
+            encoder_lstm3=LSTM(latent_dim, return_state=True, return_sequences=True,dropout=0.4,recurrent_dropout=0.4)
+            encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
+
+            # Set up the decoder, using `encoder_states` as initial state.
+            decoder_inputs = Input(shape=(None,))
+
+            #embedding layer
+            dec_emb_layer = Embedding(text_voc_size, latent_dim,trainable=True)
+            dec_emb = dec_emb_layer(decoder_inputs)
+
+            decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True,dropout=0.4,recurrent_dropout=0.4)
+            decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb,initial_state=[state_h, state_c])
+
+            # Attention layer
+            attn_layer = AttentionLayer(name='attention_layer')
+            attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+
+            # Concat attention input and decoder LSTM output
+            decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
+
+            #dense layer
+            decoder_dense =  TimeDistributed(Dense(text_voc_size, activation='softmax'))
+            decoder_outputs = decoder_dense(decoder_concat_input)
+
+            # Define the model 
+            model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+            
+            del model
+
+            from tensorflow import keras
+            model = keras.models.load_model('static/Saved_Model/my_model')
+
+            
+
+            reverse_target_word_index=text_tokenizer.index_word 
+            #reverse_source_word_index=text_tokenizer.index_word 
+            target_word_index=text_tokenizer.word_index
+
+            #target_word_index[' '] = 0
+            #reverse_target_word_index[0] = ' '
+
+
+            
+            # encoder inference
+            encoder_model = Model(inputs=encoder_inputs,outputs=[encoder_outputs, state_h, state_c])
+            # decoder inference
+            # Below tensors will hold the states of the previous time step
+            decoder_state_input_h = Input(shape=(latent_dim,))
+            decoder_state_input_c = Input(shape=(latent_dim,))
+            decoder_hidden_state_input = Input(shape=(max_text_length,latent_dim))
+
+            # Get the embeddings of the decoder sequence
+            dec_emb2= dec_emb_layer(decoder_inputs)
+
+            # To predict the next word in the sequence, set the initial states to the states from the previous time step
+            decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+            #attention inference
+            attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+            decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+            # A dense softmax layer to generate prob dist. over the target vocabulary
+            decoder_outputs2 = decoder_dense(decoder_inf_concat)
+
+            # Final decoder model
+            decoder_model = Model(
+            [decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
+            [decoder_outputs2] + [state_h2, state_c2])
+
+
+
+
+            
+            max_summary_length=68
+            def decode_sequence(input_seq):
+                # Encode the input as state vectors.
+                e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+                # Generate empty target sequence of length 1.
+                target_seq = np.zeros((1,1))
+
+                # Chose the 'start' word as the first word of the target sequence
+                target_seq[0, 0] = target_word_index[reverse_target_word_index[input_seq[0][0]]]
+                stop_condition = False
+                decoded_sentence = ''
+                SampledCompere = ''
+                sampled_token = ''
+                while not stop_condition:
+                    output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
+                    
+                    # Sample a token
+                    sampled_token_index = np.argmax(output_tokens[0, -1, :])
+
+                    SampledCompere = sampled_token
+
+                    if sampled_token_index == 0 :
+                        break
+
+                    sampled_token = reverse_target_word_index[sampled_token_index]
+
+                    if SampledCompere == sampled_token:
+                        sampled_token = ''
+
+                    if(sampled_token!='end'):
+                        decoded_sentence += ' '+sampled_token
+                        
+                        # Exit condition: either hit max length or find stop word.
+                        if (sampled_token == 'end' or len(decoded_sentence.split()) >= (max_summary_length-1)):
+                            stop_condition = True
+
+                    # Update the target sequence (of length 1).
+                    target_seq = np.zeros((1,1))
+                    target_seq[0, 0] = sampled_token_index
+
+                    # Update internal states
+                    e_h, e_c = h, c
+
+                decoded_sentence += '.'
+                
+                return decoded_sentence
+
+        
+            
+            for i in range(0,count):
+                full_article.append(decode_sequence(result[i].reshape(1,max_text_length)))
+            temp['article'] = full_article
+
    return render(request,'index.html',temp)
 
 def pdf_To_text(text):
@@ -58,8 +252,6 @@ def pdf_To_text(text):
     read_pdf = pdf.PdfFileReader(pdf_file)
     # get number of pages in pdf file
     number_of_pages = read_pdf.getNumPages()
-    #print the # of page
-    print(number_of_pages)
     
     #read page by page with getPage method
     for i in range(0,number_of_pages):
@@ -67,7 +259,7 @@ def pdf_To_text(text):
         text += p.extractText()
         
     # text arrangement
-    text = text.replace("\n", " ")
+    text = text.replace("\n", "")
     return text
 
 
